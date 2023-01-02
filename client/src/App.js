59ebd6df9f72e6c72430cc2ccec9607119ce4936
Nice <puma1800@naver.com>
2023-01-03 01:28:28 +0900

    익스프레스설정

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Heading from './Component/Heading';
+import List from './Component/List';
+import Upload from './Component/Upload';
 
 function App() {
   return (
-    <div>
-      <h2>Hello, react!</h2>
-    </div>
-      
+    <>
+    안녕안녕
+    <Heading />
+    <Routes>
+      <Route path='/list' element={<List />} />
+      <Route path='/Upload' element={<Upload />} />
+    </Routes>
+    </>
   );
 }
 
---
+++ client/src/Component/Heading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Heading() {
+    return (
+        <>
+        <h1>안녕하세요 호호호</h1>
+        </>
+    )
+}
+
+export default Heading;
